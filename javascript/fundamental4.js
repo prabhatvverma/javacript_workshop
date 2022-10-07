6d4d379d82dfb1d9652bc926a4b4c6edb5b03fa9
prabahtvverma <prabhatvverma@gmail.com>
2022-10-07 11:55:42 +0530

    lecture 4 as fundamentals4.js

--- javascript/fundamental4.js
+++ javascript/fundamental4.js
@@ -1,13 +1,36 @@
+// let printDetails = function () {
+//     console.log("......Printing Details......");
+//     console.log(this.name);
+//     console.log(this.address);
+//     console.log(this.age);
+// };
+// // printDetails.myProperty = "MY PROPERTY";
+//  console.dir(printDetails);
+// // printDetails();
+
+
+// function PersonConstructor(name, address, age) {
+//     let personObj = {};
+//     personObj.name = name;
+//     personObj.address = address;
+//     personObj.age = age;
+//     personObj.printDetails = printDetails;
+//     return personObj;
+// }
+
+// console.dir(PersonConstructor);
+// let Person1= PersonConstructor("sunil","prayagraj", 34);
+// console.dir(Person1);
+
 let printDetails = function () {
-    console.log("......Printing Details......");
+    console.log(".......printing Details......");
     console.log(this.name);
     console.log(this.address);
     console.log(this.age);
 };
+console.dir(printDetails);
 // printDetails.myProperty = "MY PROPERTY";
- console.dir(printDetails);
 // printDetails();
-
 
 function PersonConstructor(name, address, age) {
     let personObj = {};
@@ -19,5 +42,5 @@ function PersonConstructor(name, address, age) {
 }
 
 console.dir(PersonConstructor);
-let Person1= PersonConstructor("sunil","prayagraj", 34);
+let Person1 = PersonConstructor("sushil", "kanpur", 30);
 console.dir(Person1);
