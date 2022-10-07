8eee8ce8c9f0eb915ea7f2db81b3ce8876528195
prabahtvverma <prabhatvverma@gmail.com>
2022-10-07 12:53:30 +0530

    lecture 5 as fundamentals5.js (understanding this)

--- javascript/fundamental5.js
+++ javascript/fundamental5.js
@@ -1,35 +1,65 @@
-let printDetailswithArg = function (personObj) {
-    console.log("Function With Arguments");
+// let printDetailswithArg = function (personObj) {
+//     console.log("Function With Arguments");
+//     console.log(personObj.name);
+//     console.log(personObj.address);
+//     console.log(personObj.age);
+// };
+// printDetailswithArg({ name: "sunil", address: "prayagraj", age: 30 });   //Inline Object
+
+
+
+// let printDetailsWithThis = function () {
+//     console.log("Function With This");
+//     console.log(this.name);
+//     console.log(this.address);
+//     console.log(this.age);
+// }
+
+// console.dir(printDetailsWithThis);
+
+// //personObj.printDetailsWithThis();  this is the work of call function 
+// printDetailsWithThis.call({ name: "sunil", address: "prayagraj", age: 30 });
+
+
+// let person = {
+//     name: "ramratan",
+//     address: "lakhimpur",
+//     age: 30,
+//     getDetails: function () {
+//         return this.name + "-" + this.address + "-" + this.age;
+
+//     }
+// };
+
+// let details = person.getDetails();
+// console.log(details);
+
+let printDetailsWithArg = function (personObj) {
+    console.log(">>>>>>> Function with Arguments <<<<<<<<<<<");
     console.log(personObj.name);
     console.log(personObj.address);
     console.log(personObj.age);
 };
-printDetailswithArg({ name: "sunil", address: "prayagraj", age: 30 });   //Inline Object
 
-
+printDetailsWithArg({ name: "sard", address: "lakhimpur", age: 29 });
 
 let printDetailsWithThis = function () {
-    console.log("Function With This");
+    console.log(">>>>>>>> Printing Details with This <<<<<<<<<<");
     console.log(this.name);
     console.log(this.address);
     console.log(this.age);
-}
+};
 
-console.dir(printDetailsWithThis);
-
-//personObj.printDetailsWithThis();  this is the work of call function 
-printDetailsWithThis.call({ name: "sunil", address: "prayagraj", age: 30 });
-
+printDetailsWithThis.call({ name: "anjali", address: "delhi", age: 40 });
 
 let person = {
-    name: "ramratan",
-    address: "lakhimpur",
-    age: 30,
+    name: "sidharth",
+    address: "bihar",
+    age: 29,
     getDetails: function () {
-        return this.name + "-" + this.address + "-" + this.age;
-
+        return this.name + "-" + this.address + "_" + this.age;
     }
 };
 
-let details = person.getDetails();
-console.log(details);
+let Details = person.getDetails();
+console.log(Details);
