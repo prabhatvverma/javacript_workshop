5a64678c2ce343df6f5f382bd00402fe2ca077df
prabahtvverma <prabhatvverma@gmail.com>
2022-10-28 10:04:30 +0530

    dom monipulation

---
+++ javascript/fundamental14.js
@@ -0,0 +1,37 @@
+// // console.dir(document);
+// // console.dir(document.head);
+// function doSomething() {
+//     // console.log("Doing Something... .... .... ");
+//     // alert("Button Clicked")
+//     let pobj = document.createElement("p");
+//     pobj.textContent = "This pairagraph is dynamicaly created";
+//     let container = document.querySelector(".container");
+//     container.appendChild(pobj);
+// }
+
+// // function againDoSomething(){
+// //     console.log("Started Doing Something Else agian ........");
+// // }
+
+// let btnObj = document.querySelector("#clickMe");
+// console.dir(btnObj);
+// // onclick = doSomething;
+// btnObj.addEventListener("click", doSomething);
+// // btnObj.addEventListener("click", againDoSomething);
+
+
+function doSomething() {
+    let pobj = document.createElement("p");
+    pobj.textContent = "hello this is dynamicaly typed thankyou !"
+    pobj.style.color = "red"
+    pobj.style.fontSize = "1.2rem"
+    pobj.style.backgroundColor = "yellow"
+    pobj.style.padding = "16px"
+    let container = document.querySelector(".container");
+    container.appendChild(pobj);
+}
+
+let btnobj = document.querySelector("#clickMe");
+console.dir(btnobj);
+
+btnobj.addEventListener("click", doSomething);
